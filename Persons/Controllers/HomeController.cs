2b066a4ed59ae46d4bf357ae3317c4522e3f0a1f
Karol <karol.cichon17@gmail.com>
2017-04-25 12:39:12 +0200

    Add Person Project

---
+++ Persons/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using Persons.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Persons.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    
+        public ActionResult Person()
+        {
+            return View();
+
+
+
+
+            //if (!ModelState.IsValid)
+            //{
+            //    if (per.Name=="Karol")
+            //    {
+            //        ModelState.AddModelError("Name", "Proszę podać imię !");
+            //    }
+
+            //    return View(per);
+            //}
+            //else
+            //{
+            //    return View("Login");
+            //}
+            //Person osoba = new Person();
+
+            //ViewBag.Message = "Your contact page.";
+            //Persons.Models.Person person = new Models.Person();
+
+            //person.Name = "Karol";
+            //person.Surname = "Cichoń";
+            //person.Age = 26;
+
+            //Persons.Models.Person person2 = new Models.Person();
+            //person2.Name = "Aga";
+            //person2.Surname = "Kowalska";
+            //person2.Age = 21;
+
+
+        }
+
+     
+
+        [HttpPost]
+        public ActionResult Check(Person person)
+        {
+            //var imie = person.Name;
+            if (person.Name == "?")
+            {
+                ModelState.AddModelError("Name", "Nie możesz być pytajnikiem !!!");
+            }
+            if (person.Age < 18)
+            {
+                ModelState.AddModelError("Age", "Powinieneeś być dorosły !!!");
+            }
+            var porsonEmail = Models.Person.GetPersons().FirstOrDefault(prs => prs.Email == person.Email);
+
+            if (porsonEmail != null)
+            {
+                ModelState.AddModelError("Email", "Osoba z podanym mailem już istnieje");
+
+            }
+            if (!ModelState.IsValid)
+            {
+              
+                return View("Person",person);
+            }
+            else
+            {
+               
+                Models.Person.AddPerson(person);
+
+                return RedirectToAction("ShowPersons");
+            }
+        
+        }
+
+        public ActionResult ShowPersons()
+        {
+            ViewBag.Info = false;
+            var persons = Models.Person.GetPersons() ;
+
+            if (persons == null)
+            {
+                ViewBag.Info = true;
+                return RedirectToAction("Person");
+            }
+            return View(persons);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Login(Person person)
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ Persons/Models/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Persons.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Podaj imie !")]
+        [Display(Name = "Imie")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Imię powinno zawiarać minimum 3 znaki")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Podaj nazwisko !")]
+        [Display(Name = "Nazwisko")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Nazwisko powinno zawiarać minimum 5 znaków")]
+
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Podaj wiek !")]
+        [Display(Name = "Wiek")]
+        [Range(0, 65, ErrorMessage = "Zakres wieku 0 - 65. ")]
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public  string Email { get; set; }
+
+        private static List<Person> persons = new List<Person>();
+
+        public static void AddPerson(Person person)
+        {
+            Person.persons.Add(person);
+        }
+
+        public static List<Person> GetPersons()
+        {
+            return Person.persons;
+        }
+    }
+}
